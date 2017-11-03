fe1ecb8c6b2f55b5dd460965e96e27edbdac7003
Jeremy Douglass <jeremydouglass@gmail.com>
2017-11-03 13:09:51 -0700

    settings for directory locations

---
+++ settings.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+template_dir  = 'templates/topic_browser_template'
+projects_dir  = 'projects/'       # '../../write/projects'
+trash_dir     = 'projects/.Trash' # '../../write/projects/.Trash'
